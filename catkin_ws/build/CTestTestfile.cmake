a575ada74a93b5719ef3f518617759bb91971603
Kyle Kngiht <kcknigh1@gmail.com>
2020-04-07 11:22:46 -0700

    initial set up

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/source/where_am_i/catkin_ws/src
+# Build directory: /home/robond/source/where_am_i/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_robot)
